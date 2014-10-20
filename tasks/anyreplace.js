954dc7869c4f36a1e034e016833f4787a3f08da1
zhangyizhong <zhangyizhong@news.cn>
2014-10-20 11:07:20 +0800

    init

---
+++ tasks/anyreplace.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-anyreplace
+ * https://github.com/Mr1024/grunt-anyreplace
+ *
+ * Copyright (c) 2014 Mr1024
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('anyreplace', 'Replace text in files using strings, regular expressions or functions.I used to replace references from non-optimized scripts, stylesheets and other assets to their optimized version within a set of HTML files.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
